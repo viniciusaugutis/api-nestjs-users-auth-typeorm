15ef96f0b708c6e09ae8c83684569266d4a49c05
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-12-10 22:46:12 -0300

    Finalizando CRUD de usuarios

--- src/users/repositories/user.repository.ts
+++ src/users/repositories/user.repository.ts
@@ -9,9 +9,43 @@ import { User } from '../models/user.entity';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UserRole } from '../models/enums/user-roles.enum';
 import { CredentialsDto } from 'src/auth/dtos/credentials.dto';
+import { FindUsersQueryDto } from '../dtos/find-user-query.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async findUsers(
+    queryDto: FindUsersQueryDto,
+  ): Promise<{ users: User[]; total: number }> {
+    queryDto.status = queryDto.status === undefined ? true : queryDto.status;
+    queryDto.page = !queryDto.page || queryDto.page < 1 ? 1 : queryDto.page;
+    queryDto.limit =
+      !queryDto.limit || queryDto.limit > 100 ? 100 : queryDto.limit;
+
+    const { email, name, status, role } = queryDto;
+    const query = this.createQueryBuilder('user');
+    query.where('user.status = :status', { status });
+
+    if (email) {
+      query.andWhere('user.email ILIKE :email', { email: `%${email}%` });
+    }
+
+    if (name) {
+      query.andWhere('user.name ILIKE :name', { name: `%${name}%` });
+    }
+
+    if (role) {
+      query.andWhere('user.role = :role', { role });
+    }
+    query.skip((queryDto.page - 1) * queryDto.limit);
+    query.take(+queryDto.limit);
+    query.orderBy(queryDto.sort ? JSON.parse(queryDto.sort) : undefined);
+    query.select(['user.name', 'user.email', 'user.role', 'user.status']);
+
+    const [users, total] = await query.getManyAndCount();
+
+    return { users, total };
+  }
+
   async createUser(
     createUserDto: CreateUserDto,
     role: UserRole,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,12 @@ import {
   Body,
   ValidationPipe,
   UseGuards,
+  Param,
+  Get,
+  Patch,
+  ForbiddenException,
+  Delete,
+  Query,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
@@ -12,17 +18,60 @@ import { AuthGuard } from '@nestjs/passport';
 import { RolesGuard } from 'src/auth/roles.guard';
 import { UserRole } from './models/enums/user-roles.enum';
 import { Role } from 'src/auth/role.decorator';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { FindUsersQueryDto } from './dtos/find-user-query.dto';
 
 @Controller('users')
+@UseGuards(AuthGuard(), RolesGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
   @Role(UserRole.ADMIN)
-  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<User> {
     return await this.usersService.createAdminUser(createUserDto);
+  }
+
+  @Get(':id')
+  @Role(UserRole.ADMIN)
+  async findUserById(@Param('id') id): Promise<User> {
+    return await this.usersService.findUserById(id);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+    @GetUser() user: User,
+    @Param('id') id: string,
+  ) {
+    if (user.role != UserRole.ADMIN && user.id.toString() != id) {
+      throw new ForbiddenException(
+        'Você não tem autorização para acessar esse recurso',
+      );
+    } else {
+      return this.usersService.updateUser(updateUserDto, id);
+    }
+  }
+
+  @Delete(':id')
+  @Role(UserRole.ADMIN)
+  async deleteUser(@Param('id') id: string) {
+    await this.usersService.deleteUser(id);
+    return {
+      message: 'Usuário removido com sucesso',
+    };
+  }
+
+  @Get()
+  @Role(UserRole.ADMIN)
+  async findUsers(@Query() query: FindUsersQueryDto) {
+    const found = await this.usersService.findUsers(query);
+    return {
+      found,
+      message: 'Usuários encontrados',
+    };
   }
 }
