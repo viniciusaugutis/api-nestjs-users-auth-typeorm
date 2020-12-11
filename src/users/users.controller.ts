79015a60b85213bb6191befe8e6a7417206bd9d4
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-12-10 22:11:14 -0300

    Validacao no DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './models/user.entity';
@@ -8,7 +8,9 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async createAdminUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<User> {
     return await this.usersService.createAdminUser(createUserDto);
   }
 }
