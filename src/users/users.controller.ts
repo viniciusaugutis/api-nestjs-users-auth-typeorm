ede4056544847e73d817a462ae627f85c5d80f45
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-12-10 22:30:08 -0300

    Autenticacao no endpoint de criar usuario ADMIN

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './models/user.entity';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
+  @UseGuards(AuthGuard())
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<User> {
