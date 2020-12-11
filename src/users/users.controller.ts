f740e37c85fd11da69b775dc17fcd9cedc5aa5df
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-12-10 22:34:31 -0300

    Implementando autorização na API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,13 +9,17 @@ import { UsersService } from './users.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User } from './models/user.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { UserRole } from './models/enums/user-roles.enum';
+import { Role } from 'src/auth/role.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  @UseGuards(AuthGuard())
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
   ): Promise<User> {
