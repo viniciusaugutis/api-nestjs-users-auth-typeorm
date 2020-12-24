d6f1c2f991fa48332e267ba6728958232438f9fc
Vinicius Augutis <viniciusaugutis@gmail.com>
2020-12-24 10:57:12 -0300

    Logs na aplicação com winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { typeormConfig } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from './config/winston.config';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './interceptors/logger.interceptor';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeormConfig.getTypeOrmConfig()),
+    WinstonModule.forRoot(winstonConfig),
     UsersModule,
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+  ],
 })
 export class AppModule {}
